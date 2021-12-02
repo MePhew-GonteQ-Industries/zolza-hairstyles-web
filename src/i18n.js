6e504edf4cbeec2c65b52773c74a6a8ba6ee61d0
Me-Phew <basmateusz@wp.pl>
2021-12-02 15:48:47 +0100

    Change i18n config to inject translation globally for use in sfc

--- src/i18n.js
+++ src/i18n.js
@@ -21,6 +21,7 @@ function loadLocaleMessages() {
 }
 
 export default createI18n({
+  globalInjection: true,
   legacy: false,
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
