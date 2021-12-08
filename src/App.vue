b57e74bf60e2f1eadda04f9d7fdb6c877c94034d
Me-Phew <basmateusz@wp.pl>
2021-12-08 11:23:23 +0100

    Add proper no-script message

--- src/App.vue
+++ src/App.vue
@@ -22,36 +22,4 @@ export default {
 </script>
 
 <style lang="scss">
-  body {
-    background-color: #F2F2F2;
-    height: 100%;
-
-    #app {
-
-      #nav {
-        position: sticky;
-        top: 0;
-        height: 8vh;
-        z-index: 10;
-        padding-bottom: 5px;
-        background-color: $light-background;
-      }
-
-      .router-view {
-        height: 83vh;
-      }
-
-      footer {
-        height: 9vh;
-        width: 100vw;
-        padding: 5px 10% 0 10%;
-
-        @media only screen and (max-width: 400px) {
-          & {
-            padding: 5px 5% 0 5%;
-          }
-        }
-      }
-    }
-  }
 </style>
