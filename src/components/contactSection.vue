91cb263bb39ce9cbed56e81e1f88bb03b2b9d509
Me-Phew <basmateusz@wp.pl>
2021-12-09 00:43:37 +0100

    Creat completely new minimalistic footer

--- src/components/contactSection.vue
+++ src/components/contactSection.vue
@@ -1,12 +1,41 @@
 <template>
     <div class="footer-basic">
-      <ul class="list-inline">
-        <li class="list-inline-item"><router-link to="/terms">Terms of use</router-link></li>
-        <li class="list-inline-item"><router-link to="/privacyPolicy">
-          Privacy policy
-        </router-link></li>
-      </ul>
-      <p class="copyright">Zołza Hairstyles © 2021</p>
+      <div class="info-section">
+        <span>© 2021 Zołza Hairstyles</span>
+        <ul>
+          <li class="list-inline-item"><router-link to="/terms">Terms of use</router-link>
+          </li>
+          <li class="list-inline-item"><router-link to="/privacyPolicy">Privacy policy</router-link>
+          </li>
+        </ul>
+      </div>
+
+      <div class="contact">
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/Zo%C5%82za-Hairstyles-110103241392161" target="_blank">
+              <img src="@/assets/facebook_mono.svg"
+                   class="d-block" loading="lazy" decoding="async" alt="Facebook icon" width="18"
+                   height="18">
+            </a>
+            <a href="https://www.instagram.com/zolza_hairstyles/" target="_blank">
+              <img src="@/assets/instagram_mono.svg"
+                   class="d-block" loading="lazy" decoding="async" alt="Instagram icon" width="18"
+                   height="18">
+            </a>
+            <a href="tel:+48 730 601 830" target="_blank">
+              <img src="@/assets/phone.svg"
+                   class="d-block" loading="lazy" decoding="async" alt="Phone icon" width="18"
+                   height="18">
+            </a>
+            <a href="mailto: zolza.hairstyles@gmail.com" target="_blank">
+              <img id="email" src="@/assets/email.svg"
+                   class="d-block" loading="lazy" decoding="async" alt="Email icon"
+                   height="18">
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
 </template>
 
@@ -18,32 +47,92 @@ export default {
 
 <style lang='scss' scoped>
 
-  .footer-basic ul {
-    text-align:center;
-    margin-bottom:0;
-  }
+  .footer-basic {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: $footer-fg-color;
+    font-size: 14px;
 
-  .footer-basic li {
-    padding:0 10px;
-    display: inline;
-  }
+    @media only screen and (max-width: 510px) {
+      & {
+        flex-flow: column;
+      }
+    }
 
-  .footer-basic ul a {
-    font-size: 18px;
-    color:inherit;
-    text-decoration:none;
-    opacity:0.8;
-  }
+    @media only screen and (max-width: 370px) {
+      & {
+        font-size: 12px;
+      }
+    }
 
-  .footer-basic ul a:hover {
-    opacity:1;
-  }
+    .info-section {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
 
-  .footer-basic .copyright {
-    margin-top:15px;
-    text-align:center;
-    font-size:14px;
-    color:#aaa;
-    margin-bottom:0;
+    .contact {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    ul {
+      margin-left: 20px;
+
+      @media only screen and (max-width: 645px) {
+        & {
+          margin-left: 10px;
+        }
+      }
+
+      @media only screen and (max-width: 510px) {
+        & {
+          margin-left: 2px;
+        }
+      }
+
+      @media only screen and (max-width: 320px) {
+        & {
+          margin-left: 0;
+        }
+      }
+
+      li {
+        display: inline-block;
+
+        a {
+          color: $footer-fg-color;
+          font-size: 14px;
+
+          &:hover {
+            color: $hover-color;
+            text-decoration: underline;
+          }
+
+          @media only screen and (max-width: 370px) {
+            & {
+              font-size: 12px;
+            }
+          }
+
+          @media only screen and (max-width: 320px) {
+            & {
+              padding: 4px;
+            }
+          }
+
+          @media only screen and (max-width: 315px) {
+            & {
+              padding: 3px;
+            }
+          }
+        }
+      }
+    }
+
   }
 </style>
