665f0aee544eec8a1999c24e6f5616788ec309d4
Me-Phew <basmateusz@wp.pl>
2021-12-09 00:41:47 +0100

    Add new icons

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="nav">
-      <navbar />
-  </div>
+  <div id="app-wrapper" :data-theme="state.theme">
+    <div id="nav">
+      <navbar @theme-toggled="toggleTheme"/>
+    </div>
     <router-view class="router-view"/>
     <footer>
       <contactSection />
     </footer>
+  </div>
 </template>
 
 <script>
+import { reactive } from 'vue';
 import navbar from '@/components/navbar.vue';
 import contactSection from '@/components/contactSection.vue';
 
@@ -17,9 +20,67 @@ export default {
     navbar,
     contactSection,
   },
+  setup() {
+    const state = reactive({
+      theme: 'light',
+    });
+
+    function toggleTheme() {
+      switch (state.theme) {
+        case 'light':
+          state.theme = 'dark';
+          break;
+        case 'dark':
+          state.theme = 'light';
+          break;
+        default:
+          console.error('Theme was in an incorrect state');
+      }
+    }
+
+    return {
+      state,
+      toggleTheme,
+    };
+  },
 };
 
 </script>
 
 <style lang="scss">
+  #app {
+
+    #app-wrapper {
+
+      #nav {
+        position: sticky;
+        top: 0;
+        height: 8vh;
+        z-index: 10;
+      }
+
+      .router-view {
+        min-height: 85vh;
+      }
+
+      footer {
+        height: 7vh;
+        width: 100vw;
+        padding: 5px 10% 0 10%;
+        background-color: $background-accent-color;
+
+        @media only screen and (max-width: 645px) {
+          & {
+            padding: 5px 5% 0 5%;
+          }
+        }
+
+        @media only screen and (max-width: 570px) {
+          & {
+            padding: 2px 1% 0 1%;
+          }
+        }
+      }
+    }
+  }
 </style>
