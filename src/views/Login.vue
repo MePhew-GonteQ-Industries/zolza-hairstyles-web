0543ced59bb08eab1606c3641b0ac6b106d2b3c5
Me-Phew <basmateusz@wp.pl>
2021-12-09 00:52:00 +0100

    Delete old login

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="app-page" id="login-page">
+    <div class="login-section-wrapper">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style lang='scss' scoped>
+  #login-page {
+  }
+
+  .login-section-wrapper {
+    height: 75%;
+    width: 75%;
+    box-shadow: 2px 2px rgba(0, 0, 0, .5);
+  }
+</style>
