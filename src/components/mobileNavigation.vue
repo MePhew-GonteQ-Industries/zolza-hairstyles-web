ac61a9356b71f889512ed6da38e09e3164ca2db8
Me-Phew <basmateusz@wp.pl>
2021-12-09 00:46:28 +0100

    Fix overlapping issues, add transition to menu links, add logo on top of the menu

--- src/components/mobileNavigation.vue
+++ src/components/mobileNavigation.vue
@@ -1,9 +1,12 @@
 <template>
       <div class="mobile-navigation-container"
-      :class="{ 'mobile-menu-hidden' : !mobileMenuActive,
+      :class="{ 'mobile-menu-collapsed' : !mobileMenuActive,
       'mobile-menu-shown' : mobileMenuActive}">
         <nav class="mobile-navigation">
-          <ul class="primary-nav">
+          <img src="@/assets/wordmark.svg" alt="" class="wordmark">
+
+          <ul class="primary-nav" :class="{'hidden' : !mobileMenuActive,
+                                          'shown' : mobileMenuActive}">
             <li><router-link to="/"
                @click="toggleMobileMenu">Home</router-link></li>
             <li><router-link to="/services"
@@ -12,7 +15,8 @@
                @click="toggleMobileMenu">Contact</router-link></li>
           </ul>
 
-          <ul class="secondary-nav">
+          <ul class="secondary-nav" :class="{'hidden' : !mobileMenuActive,
+                                          'shown' : mobileMenuActive}">
             <li><router-link to="/login"
                @click="toggleMobileMenu">Log In</router-link></li>
             <li><router-link to="/signup"
@@ -27,7 +31,7 @@
 export default {
   name: 'mobileNavigation',
   props: {
-    mobileMenuActive: { default: false }, // TODO: Add transition : line 5
+    mobileMenuActive: { default: false },
   },
   setup(props, ctx) {
     function toggleMobileMenu() {
@@ -40,52 +44,93 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.mobile-menu-hidden {
-  width: 0;
-}
+  .hidden {
+    visibility: hidden;
+  }
 
-.mobile-menu-shown {
-    width: 30%;
-}
+  .shown {
+    visibility: visible;
+  }
 
-.mobile-navigation-container {
-  position: absolute;
-  z-index: 20;
-  height: 100vh;
-  left: 0;
-  top: 0;
-  background-color: $darker-white;
-  transition: width 500ms;
-  overflow: hidden;
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
+  ul {
+    transition: visibility 200ms;
+  }
 
-  .mobile-navigation {
-    padding: 80px 0 20px;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: right;
+  .wordmark {
+    position: absolute;
+    left: 150px;
+    top: 10px;
+    height: 50px;
+    filter: $logo-filter;
 
-    a {
-      margin-left: 10px;
-      font-size: 25px;
-      transition: visibility 5000ms;
+    @media only screen and (max-width: 620px) {
+      & {
+        top: 12px;
+        left: 70px;
+        height: 40px;
+      }
+    }
+
+    @media only screen and (max-width: 410px) {
+      & {
+        top: 16px;
+        height: 35px;
+      }
+    }
+
+    @media only screen and (max-width: 375px) {
+      & {
+        display: none;
+      }
     }
   }
-  @media only screen and (max-width: 990px) {
-      .secondary-nav {
-       display: none;
-      }
-    }
-  @media only screen and (max-width: 570px) {
-      .secondary-nav {
-        display: initial;
-      }
-    }
-}
 
+  .mobile-menu-collapsed {
+    width: 0;
+  }
+
+  .mobile-menu-shown {
+      width: 60%; // TODO: Fix secondary nav overlapping
+  }
+
+  .mobile-navigation-container {
+    position: absolute;
+    z-index: 20;
+    height: 100vh;
+    left: 0;
+    top: 0;
+    transition: width 500ms;
+    overflow: hidden;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    background-color: $background-accent-color;
+
+    .mobile-navigation {
+      margin-top: 8vh;
+      padding: 30px 0 90px;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      background-color: $background-color;
+
+      a {
+        color: $primary-color;
+        margin-left: 20px;
+        font-size: 25px;
+      }
+    }
+    @media only screen and (max-width: 990px) {
+        .secondary-nav {
+         display: none;
+        }
+      }
+    @media only screen and (max-width: 570px) {
+        .secondary-nav {
+          display: initial;
+        }
+      }
+  }
 </style>
