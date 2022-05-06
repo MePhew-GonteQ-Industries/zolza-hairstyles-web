f7bfd0fd6b7a99745cbf2608354dfb2c72b02648
Me-Phew <basmateusz@wp.pl>
2022-05-06 22:23:02 +0200

    Add custom button component

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,13 +1,32 @@
 <template>
-  <button>Zaloguj się</button>
+  <button class="login">{{ content }}</button>
 </template>
 
 <script>
 export default {
   name: 'CustomButton',
+  props: {
+    content: String,
+  },
 };
 </script>
 
 <style lang='scss' scoped>
+  button {
+    height: 60px;
+    width: 200px;
+    border-radius: 30px;
+    border: 2px solid transparent;
+    background-color: #1A92F0;
+    color: white;
+    font-size: 1.2rem;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    cursor: pointer;
 
+    &:hover {
+      border-color: white;
+      box-shadow: 0 0 4px 3px rgb(156, 156, 156);
+    }
+  }
 </style>
