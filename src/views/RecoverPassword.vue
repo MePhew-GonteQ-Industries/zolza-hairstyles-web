5e04067554193c810a527bf22a2ad8a4cca17468
Me-Phew <basmateusz@wp.pl>
2022-05-08 16:45:15 +0200

    Add password recovery page

---
+++ src/views/RecoverPassword.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="app-page" id="recover-password-page">
+    <div class="recovery-wrapper">
+      <p>Enter your email address and we will send you a password reset link</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'RecoverPassword',
+};
+</script>
+
+<style>
+</style>
