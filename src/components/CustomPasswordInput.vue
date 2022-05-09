fc96f51db5e5e316d1cb20dbfb8a65206715d0cb
Me-Phew <basmateusz@wp.pl>
2022-05-10 00:00:08 +0200

    Create custom password input component

---
+++ src/components/CustomPasswordInput.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="password-input-wrapper">
+    <input :autocomplete="autocomplete"
+    class="password-input"
+    :type="passwordHidden ? 'password' : 'text'"
+    name="password" placeholder="Password" v-model="password"
+    @input="event => $emit('update:password', event.target.value)">
+    <label for="password">{{ label }}</label>
+    <div class="show-password" @click="showPassword">
+      <img class="eye-icon" v-if="passwordHidden"
+      src="@/assets/eye-crossed-out.svg" alt="show-password-icon">
+      <img class="eye-icon" v-if="!passwordHidden"
+      src="@/assets/eye.svg" alt="hide-password-icon">
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'CustomPasswordInput',
+  props: {
+    autocomplete: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: 'Password',
+    },
+  },
+  setup() {
+    const password = ref('');
+    const passwordHidden = ref('false');
+
+    function showPassword() {
+      passwordHidden.value = !passwordHidden.value;
+    }
+
+    return {
+      password,
+      passwordHidden,
+      showPassword,
+    };
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.password-input-wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  label {
+    color: #84868f;
+    position: absolute;
+    left: 30px;
+    transition: all 0.5s;
+    pointer-events: none;
+    backface-visibility: hidden;
+    transform-origin: 0 0;
+  }
+
+  .show-password {
+    position: absolute;
+    right: 23px;
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    outline: none;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+
+    img {
+      width: 30px;
+      height: 30px;
+    }
+
+    &:hover {
+      background-color: #2b2d33;
+    }
+
+    &:active {
+      background-color: #55575e;
+    }
+
+    .eye-icon {
+      width: 30px;
+      height: 30px;
+      user-select: none;
+    }
+  }
+
+  input {
+    padding-top: 20px;
+    padding-left: 30px;
+    padding-right: 55px;
+    border: 2px solid transparent;
+    background-color: #323644;
+    color: white;
+    height: 65px;
+    width: 420px;
+    border-radius: 15px;
+    box-shadow: none;
+    transition: all .3s, letter-spacing .6s;
+
+    &:-webkit-autofill{
+      -webkit-text-fill-color: white;
+      color: white;
+      font-family: 'Open Sans', sans-serif;
+    }
+
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus {
+      transition: background-color 5000s ease-in-out 0s,
+      border-color .3s, box-shadow .3s, letter-spacing .6s;
+    }
+
+    &:focus, &:hover {
+      border-color: #3e83ba;
+      background-color: #3d4049;
+      box-shadow: 0 0 0px 3px #274b6d;
+      outline: none;
+    }
+
+    &[type="password"] {
+      letter-spacing: 4px;
+    }
+
+    &:focus, &:hover {
+      border-color: #3e83ba;
+      background-color: #3d4049;
+      box-shadow: 0 0 0px 3px #274b6d;
+      outline: none;
+    }
+
+    &:focus + label,
+    &:not(:placeholder-shown) + label,
+    &:-webkit-autofill:active + label  {
+      color: #00a2e8;
+      transform: translateY(-0.6em) scale(0.8);
+    }
+
+    &::placeholder {
+      color: transparent;
+      user-select: none;
+    }
+  }
+}
+</style>
