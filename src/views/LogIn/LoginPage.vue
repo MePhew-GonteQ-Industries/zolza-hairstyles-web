d7c85c74f3589cf2f97543e89424a9cda2ea51bb
Me-Phew <basmateusz@wp.pl>
2022-05-09 23:59:47 +0200

    Utilize new input components

--- src/views/LogIn/LoginPage.vue
+++ src/views/LogIn/LoginPage.vue
@@ -1,30 +1,15 @@
 <template>
   <section class="app-page" id="login-page">
-    <h1>Login</h1>
-    <div class="email-confirmation-message" :class="{ hide: !emailConfirmationMessageShown }">
+    <h1>Log In to Zołza Hairstyles</h1>
+    <div class="email-confirmation-message" v-if="emailConfirmationMessageShown">
         <h4>Email address has been confirmed successfully</h4>
         <h4>You can now login to your account</h4>
     </div>
     <form class="login-form" @submit.prevent="loginUser">
-      <div class="input-wrapper">
-        <input class="email-input" type="text" name="email"
-        placeholder="Email" v-model="userData.email">
-        <label class="email-label" for="email">Email</label>
-        <img class="email-icon" src="@/assets/footer/email.svg" alt="">
-      </div>
-      <div class="input-wrapper">
-        <input class="password-input" :type="passwordHidden ? 'password' : 'text'" name="password"
-         placeholder="Password" v-model="userData.password">
-        <label class="password-label" for="password">Password</label>
-        <div class="show-password" @click="showPassword">
-            <img class="eye-icon" :class="{ hide: !passwordHidden }"
-            src="@/assets/eye-crossed-out.svg" alt="show-password-icon">
-            <img class="eye-icon" :class="{ hide: passwordHidden }"
-            src="@/assets/eye.svg" alt="hide-password-icon">
-        </div>
-      </div>
+      <CustomInput label='Email' :iconSrc='emailIcon' autocomplete="email"/>
+      <CustomPasswordInput autocomplete="current-password"/>
       <router-link class="forgot-password-link" to="/recover-password">
-        Forgot password?</router-link>
+      Forgot password?</router-link>
       <CustomButton content="Login"/>
     </form>
     <p>{{message}}</p>
@@ -35,6 +20,9 @@
 import { ref } from 'vue';
 import axios from 'axios';
 import CustomButton from '../../components/CustomButton.vue';
+import CustomInput from '@/components/CustomInput.vue';
+import CustomPasswordInput from '@/components/CustomPasswordInput.vue';
+import emailIcon from '@/assets/email.svg';
 
 export default {
   name: 'LoginPage',
@@ -103,6 +91,7 @@ export default {
       passwordHidden,
       showEmailConfirmationMessage,
       emailConfirmationMessageShown,
+      emailIcon,
     };
   },
   mounted() {
@@ -111,7 +100,11 @@ export default {
       this.showEmailConfirmationMessage();
     }
   },
-  components: { CustomButton },
+  components: {
+    CustomButton,
+    CustomInput,
+    CustomPasswordInput,
+  },
 };
 </script>
 
@@ -122,7 +115,8 @@ export default {
     justify-content: flex-start;
 
     h1 {
-      font-size: 5rem;
+      font-size: 2rem;
+      font-weight: 300;
     }
   }
 
@@ -151,10 +145,6 @@ export default {
     background-color: #DFF2BF;
   }
 
-  .hide {
-    display: none;
-  }
-
   .login-form {
     display: flex;
     flex-direction: column;
@@ -165,111 +155,5 @@ export default {
     background-color: rgba(39, 42, 54, .6);
     padding: 100px 0 100px 0;
     border-radius: 20px;
-
-    label {
-        color: #84868F;
-        position: absolute;
-        transform-origin: 0 0;
-        transform: translateX(-8.9em);
-        transition: all .5s;
-        pointer-events: none;
-      }
-
-    .email-label {
-      transform: translateX(-9.9em);
-    }
-
-    .input-wrapper {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-
-      .email-icon {
-        position: absolute;
-        transform: translateX(11em);
-        height: 30px;
-        width: 30px;
-      }
-
-      .show-password {
-        position: absolute;
-        width: 45px;
-        height: 45px;
-        border-radius: 50%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        transform: translateX(175px);
-        outline: none;
-        border: none;
-        background-color: transparent;
-        cursor: pointer;
-
-        &:hover {
-           background-color: #2b2d33;
-        }
-
-        &:active {
-          background-color: #55575e;
-        }
-
-        .eye-icon {
-          width: 30px;
-          height: 30px;
-        }
-      }
-    }
-
-    .password-input {
-      transition: letter-spacing .6s;
-    }
-
-    .password-input[type='password'] {
-      letter-spacing: 4px;
-    }
-
-    input {
-      padding-left: 30px;
-      padding-right: 55px;
-      outline: none;
-      border: none;
-      border: 2px solid transparent;
-      background-color: #323644;
-      color: white;
-      height: 65px;
-      width: 420px;
-      border-radius: 15px;
-      box-shadow: none;
-      transition: all .3s;
-      font-weight: bolder;
-      font-size: .9rem;
-      padding-top: 20px;
-
-      &:focus {
-        border-color: #3E83BA;
-        background-color: #3D4049;
-        box-shadow: 0 0 0px 3px #274B6D;
-        outline: none;
-        }
-
-      &:focus + label {
-        color: #00A2E8;
-      }
-
-      &:focus + .password-label,
-      &:not(:placeholder-shown) + .password-label {
-        transform: translateX(-8.9em) translateY(-.6em) scale(.8);
-      }
-
-      &:focus + .email-label,
-      &:not(:placeholder-shown) + .email-label {
-        transform: translateX(-9.9em) translateY(-.6em) scale(.8);
-      }
-
-      &::placeholder {
-        color: transparent;
-        user-select: none;
-        }
-    }
   }
 </style>
