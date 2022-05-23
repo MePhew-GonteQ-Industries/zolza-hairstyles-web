6a2921d52ee14e9aea70f80120bdbd9c98c89f3d
Me-Phew <basmateusz@wp.pl>
2022-05-24 00:14:22 +0200

    Allow login functionality

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,15 +5,7 @@ export default {
     loggedIn: false,
     accessToken: '',
     refreshToken: '',
-    userData: {
-      email: '',
-      name: '',
-      surname: '',
-      gender: '',
-      verified: false,
-      permission_level: [],
-      created_at: null,
-    },
+    userData: null,
   },
 
   getters: {
@@ -26,47 +18,28 @@ export default {
       state.refreshToken = loginData.refresh_token;
     },
 
+    relogin(state, loginData) {
+      state.loggedIn = true;
+      state.accessToken = loginData.accessToken;
+      state.refreshToken = loginData.refreshToken;
+    },
+
     logout(state) {
       state.loggedIn = false;
       state.accessToken = '';
       state.refreshToken = '';
+      state.userData = null;
     },
 
     setUserData(state, userData) {
-      console.log(userData);
       state.userData = userData;
     },
   },
 
   actions: {
-    checkUserData({ commit }) {
-      axios.get('users/me').then((response) => {
-        commit('setUserData', response.data);
-      })
-        .catch((error) => {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            switch (error.response.status) {
-              case 404: {
-                // message.value = 'Incorrect user data';
-                break;
-              }
-              default: {
-                console.error(error.response.status);
-                break;
-              }
-            }
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-          }
-        });
+    async checkUserData({ commit }) {
+      const response = await axios.get('users/me');
+      commit('setUserData', response.data);
     },
 
     setAuthHeader({ state }) {
@@ -74,43 +47,69 @@ export default {
         state.accessToken}`;
     },
 
-    login({ commit, dispatch }, userData) {
-      return new Promise((resolve, reject) => {
-        axios.post('auth/login', new URLSearchParams({
-          grant_type: 'password',
-          username: userData.email,
-          password: userData.password,
-        }), {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        }).then((response) => {
-          console.log(response);
-          commit('login', response.data);
-          dispatch('setAuthHeader');
-          dispatch('checkUserData');
-          resolve();
-        })
-          .catch((error) => {
-            if (error.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              reject(error.response.status);
-            } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
-              reject(error.request);
-            } else {
-              // Something happened in setting up the request that triggered an Error
-              reject(error.message);
-            }
-            reject();
-          });
-      });
+    loadUser({ commit }) {
+      if (localStorage.getItem('loginData') && localStorage.getItem('userData')) {
+        commit('relogin', JSON.parse(localStorage.getItem('loginData')));
+        commit('setUserData', JSON.parse(localStorage.getItem('userData')));
+      }
     },
 
-    logout({ commit }) {
+    saveUserData({ state }) {
+      localStorage.setItem('loginData', JSON.stringify(
+        {
+          accessToken: state.accessToken,
+          refreshToken: state.refreshToken,
+        },
+      ));
+      localStorage.setItem('userData', JSON.stringify(
+        state.userData,
+      ));
+    },
+
+    async login({ commit, dispatch }, userData) {
+      axios.post('auth/login', new URLSearchParams({
+        grant_type: 'password',
+        username: userData.email,
+        password: userData.password,
+      }), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }).then(async (response) => {
+        commit('login', response.data);
+        await dispatch('setAuthHeader');
+        await dispatch('checkUserData');
+        await dispatch('saveUserData');
+      })
+        .catch((error) => {
+          if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+            throw new Error(error.response.status);
+          } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+            throw new Error(error.reques);
+          } else {
+          // Something happened in setting up the request that triggered an Error
+            throw new Error(error.message);
+          }
+        });
+    },
+
+    removeAuthHeader() {
+      axios.defaults.headers.common.Authorization = '';
+    },
+
+    deleteUser() {
+      localStorage.removeItem('loginData');
+      localStorage.removeItem('userData');
+    },
+
+    logout({ commit, dispatch }) {
+      dispatch('deleteUser');
+      dispatch('removeAuthHeader');
       commit('logout');
     },
 
