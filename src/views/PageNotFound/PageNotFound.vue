1b087d81b8d56e2795c5b4eb321217ed5bd253c3
biQte <patrykgonet@gmail.com>
2022-06-19 11:52:24 +0200

    Basic 404 page

--- src/views/PageNotFound/PageNotFound.vue
+++ src/views/PageNotFound/PageNotFound.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="app-page" id="page-not-found">
-    <h1>Nie znaleziono strony</h1>
+    <h1>404</h1>
+    <h3>Nie znaleziono strony</h3>
   </section>
 </template>
 
@@ -11,5 +12,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import url('https://fonts.googleapis.com/css2?family=Exo+2:wght@400;700&display=swap');
+  #page-not-found{
+    background-image: url('~@/assets/blob-background.svg');
+    background-size: cover;
+    justify-content: center;
+    h1{
+      font-size: 30em;
+      font-family: 'Exo 2', sans-serif;
+    }
+    h3{
+      font-size: 8em;
+    }
+  }
 </style>>
