0eb214a6e63c3c81c74c4d3ed6426bf7bf7624ac
biQte <patrykgonet@gmail.com>
2022-06-21 19:28:31 +0200

    Add photos with basic layout on homepage but links from facebook

---
+++ src/components/Carousel/CarouselComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="carousel">
+        <div class="carousel-inner">
+            <CarouselItem
+            v-for="(photo, index) in photos"
+            :key="`photo-${index}`"
+            :photo="photo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import CarouselItem from './CarouselItem.vue';
+
+export default {
+  name: 'CarouselComponent',
+  props: ['photos'],
+  components: { CarouselItem },
+};
+</script>
