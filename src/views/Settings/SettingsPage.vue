b03045fa0ee9bcd99009cff96b6ee320cfbe1ba7
Me-Phew <basmateusz@wp.pl>
2022-07-16 14:06:30 +0200

    Add proper settings page

--- src/views/Settings/SettingsPage.vue
+++ src/views/Settings/SettingsPage.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="app-page" id="notification-settings-page">
-    <h1>This is a notifications settings page</h1>
+    <h1>This is a settings page</h1>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'NotificationSettings',
+  name: 'SettingsPage',
 };
 </script>
 
