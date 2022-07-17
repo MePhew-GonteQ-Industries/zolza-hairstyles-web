ae0e0bca29b7e33278c762eb7b1207c18dc11ebf
Me-Phew <basmateusz@wp.pl>
2022-07-17 13:07:42 +0200

    Add custom checkbox component

---
+++ src/components/CustomCheckbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="checkbox-wrapper">
+    <input :aria-checked="checked" role="checkbox" type="checkbox" :checked="checked"
+    @change="event => $emit('update:checked', event.target.checked)">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomCheckbox',
+  emits: [
+    'update:checked',
+  ],
+  props: {
+    checked: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
