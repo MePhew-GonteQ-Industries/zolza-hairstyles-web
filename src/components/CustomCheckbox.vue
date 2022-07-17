5cfc2c1743fd3e4eb2c2b9531ea4432820381661
Me-Phew <basmateusz@wp.pl>
2022-07-17 13:59:42 +0200

    Improve custom checkbox

--- src/components/CustomCheckbox.vue
+++ src/components/CustomCheckbox.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="checkbox-wrapper">
+    <i class="ph-check-bold checkbox-icon" v-if="checked"></i>
     <input :aria-checked="checked" role="checkbox" type="checkbox" :checked="checked"
-    @change="event => $emit('update:checked', event.target.checked)">
+    @change="event => $emit('update:checked', event.target.checked)"
+    name='custom-checkbox' :id="checkboxId">
+    <label :for="checkboxId">
+      <slot></slot>
+    </label>
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+
 export default {
   name: 'CustomCheckbox',
   emits: [
@@ -17,8 +25,58 @@ export default {
       required: true,
     },
   },
+  setup() {
+    const checkboxId = ref(null);
+
+    onMounted(() => {
+      checkboxId.value = uuidv4();
+    });
+
+    return {
+      checkboxId,
+    };
+  },
 };
 </script>
 
 <style lang='scss' scoped>
+.checkbox-wrapper {
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+  position: relative;
+  height: 20px;
+
+  .checkbox-icon {
+    position: absolute;
+    left: 10px;
+    top: 10px;
+    transform: translate(-50%, -50%);
+    font-size: 1rem;
+    color: $primary-color;
+    z-index: 1;
+    pointer-events: none;
+  }
+
+  input {
+    position: relative;
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    background-color: transparent;
+    border: 2px solid $primary-text-color;
+    border-radius: .2rem;
+  }
+
+  input:checked {
+    background-color: $accent-color;
+    border-color: transparent;
+  }
+
+  label {
+    cursor: pointer;
+    user-select: none;
+  }
+}
 </style>
