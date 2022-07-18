de746d954e3deed81dc37c743a04bfef76a7657e
Me-Phew <basmateusz@wp.pl>
2022-07-19 00:06:52 +0200

    Improve styling

--- src/views/SignUp/SignUpPage.vue
+++ src/views/SignUp/SignUpPage.vue
@@ -1,79 +1,105 @@
 <template>
-  <section class="app-page" id="sign-up-page">
-    <div class="sign-up-wrapper">
-      <h1>{{t('signUp.heading')}}</h1>
+  <section class="app-page form-page">
+    <div class="form-wrapper">
+      <h1>{{ t("signUp.heading") }}</h1>
+      <h3>{{ t("signUp.subtitle") }}</h3>
 
-      <form class="sing-up-form" @submit.prevent="handleSubmit" novalidate>
-        <input class="hidden-input" type="text" autocomplete="username">
-
+      <form @submit.prevent="handleSubmit" novalidate>
         <div class="inputs">
-          <CustomInput :label="t('signUp.nameField.label')"
-          iconClass='ph-identification-card-light' autocomplete="given-name"
-          :required='true' :messageEmpty="t('signUp.nameField.messageEmpty')"
-          :messageInvalid="t('signUp.nameField.messageInvalid')"
-          v-model:value="userData.name"
-          :forceValidate="forceValidate" :invalid="!userData.name"/>
+          <input class="hidden-input" type="text" autocomplete="username" />
 
-          <CustomInput :label="t('signUp.surnameField.label')"
-          iconClass='ph-identification-card-light'
-          autocomplete="family-name"
-          v-model:value="userData.surname"
-          :required='true' :messageEmpty="t('signUp.surnameField.messageEmpty')"
-          :messageInvalid="t('signUp.surnameField.messageInvalid')"
-          :invalid="!userData.surname" :forceValidate="forceValidate"/>
+          <div class="names">
+            <CustomInput
+              :label="t('signUp.nameField.label')"
+              iconClass="ph-identification-card-light"
+              autocomplete="given-name"
+              :required="true"
+              :messageEmpty="t('signUp.nameField.messageEmpty')"
+              :messageInvalid="t('signUp.nameField.messageInvalid')"
+              v-model:value="userData.name"
+              :forceValidate="forceValidate"
+              :invalid="!userData.name"
+            />
 
-          <CustomInput :label="t('signUp.emailField.label')"
-          iconClass='ph-envelope-simple-light'
-          autocomplete="email" inputType='email'
-          v-model:value="userData.email"
-          :required='true' :messageEmpty="t('signUp.emailField.messageEmpty')"
-          :messageInvalid="t('signUp.emailField.messageInvalid')"
-          :invalid="emailInvalid" :forceValidate="forceValidate"/>
+            <CustomInput
+              :label="t('signUp.surnameField.label')"
+              iconClass="ph-identification-card-light"
+              autocomplete="family-name"
+              v-model:value="userData.surname"
+              :required="true"
+              :messageEmpty="t('signUp.surnameField.messageEmpty')"
+              :messageInvalid="t('signUp.surnameField.messageInvalid')"
+              :invalid="!userData.surname"
+              :forceValidate="forceValidate"
+            />
+          </div>
 
-          <CustomSelect class="selector"
-          :header="t('signUp.genderField.header')"
-          iconClass="ph-gender-intersex-light"
-          :options="genderOptions"
-          v-model:selected-value="userData.gender"
-          :required='true' :messageEmpty="t('signUp.genderField.messageEmpty')"
-          :messageInvalid="t('signUp.genderField.messageInvalid')"
-          :invalid="!userData.gender" :forceValidate="forceValidate"/>
+          <CustomInput
+            :label="t('signUp.emailField.label')"
+            iconClass="ph-envelope-simple-light"
+            autocomplete="email"
+            inputType="email"
+            v-model:value="userData.email"
+            :required="true"
+            :messageEmpty="t('signUp.emailField.messageEmpty')"
+            :messageInvalid="t('signUp.emailField.messageInvalid')"
+            :invalid="emailInvalid"
+            :forceValidate="forceValidate"
+          />
 
-          <CustomInput :label="t('signUp.passwordField.label')"
-          autocomplete="new-password"
-          type='password'
-          v-model:value="userData.password"
-          :invalid="!passwordRepeat || passwordRepeat !== userData.password"
-          :forceValidate="forceValidate"
-          :required='true' :messageEmpty="t('signUp.passwordField.messageEmpty')"
-          :messageInvalid="t('signUp.passwordField.messageInvalid')"/>
+          <CustomSelect
+            class="select"
+            :header="t('signUp.genderField.header')"
+            iconClass="ph-gender-intersex-light"
+            :options="genderOptions"
+            v-model:selected-value="userData.gender"
+            :required="true"
+            :messageEmpty="t('signUp.genderField.messageEmpty')"
+            :messageInvalid="t('signUp.genderField.messageInvalid')"
+            :invalid="!userData.gender"
+            :forceValidate="forceValidate"
+          />
 
-          <CustomInput :label="t('signUp.repeatPasswordField.label')"
-          autocomplete="new-password"
-          type='password'
-          v-model:value="passwordRepeat"
-          :invalid="!passwordRepeat || passwordRepeat !== userData.password"
-          :forceValidate="forceValidate"
-          :required='true' :messageEmpty="t('signUp.repeatPasswordField.messageEmpty')"
-          :messageInvalid="t('signUp.repeatPasswordField.messageInvalid')"/>
+          <CustomInput
+            :label="t('signUp.passwordField.label')"
+            autocomplete="new-password"
+            type="password"
+            v-model:value="userData.password"
+            :invalid="!passwordRepeat || passwordRepeat !== userData.password"
+            :forceValidate="forceValidate"
+            :required="true"
+            :messageEmpty="t('signUp.passwordField.messageEmpty')"
+            :messageInvalid="t('signUp.passwordField.messageInvalid')"
+          />
+
+          <CustomInput
+            :label="t('signUp.repeatPasswordField.label')"
+            autocomplete="new-password"
+            type="password"
+            v-model:value="passwordRepeat"
+            :invalid="!passwordRepeat || passwordRepeat !== userData.password"
+            :forceValidate="forceValidate"
+            :required="true"
+            :messageEmpty="t('signUp.repeatPasswordField.messageEmpty')"
+            :messageInvalid="t('signUp.repeatPasswordField.messageInvalid')"
+          />
         </div>
 
-        <div class="under-form-section">
-          <CustomCheckbox>Akceptuję
-            <router-link to="/">
-              regulamin serwisu
-            </router-link>
+        <div class="under-inputs-section">
+          <CustomCheckbox v-model:checked="termsAccepted"
+            >Akceptuję
+            <router-link to="/terms-of-use"> regulamin serwisu </router-link>
             i
-            <router-link to="/">
+            <router-link to="/privacy-policy">
               politykę prywatności
             </router-link>
           </CustomCheckbox>
         </div>
 
-        <CustomButton class="sign-up-button">Utwórz konto</CustomButton>
+        <CustomButton class="btn">Utwórz konto</CustomButton>
       </form>
-      <div class="login-redirection">
-        <h3>Masz już konto?</h3>
+      <div class="under-form-section">
+        <span>Masz już konto?</span>
         <router-link to="/">Zaloguj się</router-link>
       </div>
     </div>
@@ -115,6 +141,8 @@ export default {
     const passwordScore = ref(null);
 
     const emailInvalid = computed(() => !validateEmail(userData.value.email));
+
+    const termsAccepted = ref(false);
 
     function onScore(payload) {
       passwordScore.value = payload.score;
@@ -169,17 +197,20 @@ export default {
     }
 
     function signUpUser() {
-      axios.post('users/register',
-        userData.value,
-        {
+      axios
+        .post('users/register', userData.value, {
           headers: {
             'content-language': navigator.language,
             'preferred-theme': 'dark',
           },
-        }).then((response) => {
-        const { email } = response.data;
-        router.push({ name: 'Login', params: { email, accountCreated: true } });
-      })
+        })
+        .then((response) => {
+          const { email } = response.data;
+          router.push({
+            name: 'Login',
+            params: { email, accountCreated: true },
+          });
+        })
         .catch((error) => {
           if (error.response) {
             // The request was made and the server responded with a status code
@@ -247,75 +278,11 @@ export default {
       passwordScore,
       emailInvalid,
       forceValidate,
+      termsAccepted,
     };
   },
 };
 </script>
 
 <style lang='scss' scoped>
-#sign-up-page {
-
-  .sign-up-wrapper{
-    text-align: center;
-    background-color: $secondary-color;
-    border-radius: .375rem;
-    box-shadow: 0 0 8px -2px $box-shadow-color;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: .9375rem .875rem;
-    h1 {
-      font-size: 1.5rem;
-    }
-    h4{
-      font-size: .875rem;
-    }
-
-    .sing-up-form {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      padding: 30px 20px 0 20px;
-
-      .inputs{
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-      }
-
-      .under-form-section{
-        margin-top: 10px;
-        width: 100%;
-        font-size: 1em;
-        display: flex;
-        align-items: flex-start;
-        a{
-          font-size: 1em;
-          color: $accent-color;
-          &:hover{
-            text-decoration: underline;
-          }
-        }
-      }
-      .sign-up-button{
-        margin: 30px 0;
-      }
-
-      .hidden-input {
-        display: none;
-      }
-    }
-    .login-redirection{
-      font-size: .8rem;
-      margin: auto;
-      display: flex;
-      align-items: center;
-      a{
-        color: $accent-color;
-        font-size: .8rem;
-      }
-    }
-  }
-}
 </style>
