1793b4895f96a535b40bd4e33216ae6d2362401b
Me-Phew <basmateusz@wp.pl>
2022-07-19 19:52:30 +0200

    Add functions for getting and setting css property values

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,14 @@
-export default function validateEmail(email) {
+function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
+
+function getCssPropertyValue(propertyName) {
+  return getComputedStyle(document.documentElement).getPropertyValue(propertyName);
+}
+
+function setCssPropertyValue(element, propertyName, propertyValue) {
+  element.style.setProperty(propertyName, propertyValue);
+}
+
+export { validateEmail, getCssPropertyValue, setCssPropertyValue };
