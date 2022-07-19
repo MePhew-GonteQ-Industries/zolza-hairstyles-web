4be33de333ed735ba079bdbcd8b519a67f12ab2e
Me-Phew <basmateusz@wp.pl>
2022-07-19 17:35:56 +0200

    Implement generic side menu navigation

--- src/views/Dashboard/DashboardPage.vue
+++ src/views/Dashboard/DashboardPage.vue
@@ -1,26 +1,71 @@
 <template>
   <section class="app-page" id="dashboard-page">
-      <DasboardNavigation class='dashboard-navigation'/>
-      <router-view></router-view>
+    <SideMenu :links="links"/>
+    <router-view></router-view>
   </section>
 </template>
 
 <script>
-import DasboardNavigation from '@/views/Dashboard/DasboardNavigation.vue';
+import { v4 as uuidv4 } from 'uuid';
+import SideMenu from '@/components/SideMenu.vue';
 
 export default {
   name: 'DashboardPage',
   components: {
-    DasboardNavigation,
+    SideMenu,
+  },
+  setup() {
+    const links = [
+      {
+        id: uuidv4(),
+        toName: 'summaryView',
+        iconClass: 'ph-table-light',
+        text: 'Podsumowanie',
+      },
+      {
+        id: uuidv4(),
+        toName: 'appointmentsManagement',
+        iconClass: 'ph-bookmarks-light',
+        text: 'Wizyty',
+      },
+      {
+        id: uuidv4(),
+        toName: 'servicesManagement',
+        iconClass: 'ph-scissors-light',
+        text: 'Usługi',
+      },
+      {
+        id: uuidv4(),
+        toName: 'usersManagement',
+        iconClass: 'ph-users-light',
+        text: 'Użytkownicy',
+      },
+      {
+        id: uuidv4(),
+        toName: 'workHoursManagement',
+        iconClass: 'ph-clock-light',
+        text: 'Godziny pracy',
+      },
+      {
+        id: uuidv4(),
+        toName: 'statsView',
+        iconClass: 'ph-chart-line-light',
+        text: 'Statystyki',
+      },
+      {
+        id: uuidv4(),
+        toName: 'serviceSettings',
+        iconClass: 'ph-gear-six-light',
+        text: 'Ustawienia',
+      },
+    ];
+
+    return {
+      links,
+    };
   },
 };
 </script>
 
 <style lang="scss">
-#dashboard-page {
-  display: grid;
-  grid-template-columns: 1fr 4fr;
-  align-items: stretch;
-  background-color: $secondary-color;
-}
 </style>
