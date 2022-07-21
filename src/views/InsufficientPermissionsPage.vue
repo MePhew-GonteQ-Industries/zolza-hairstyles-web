b942a05638d1cf28139138b4c458efe4eac128bb
Me-Phew <basmateusz@wp.pl>
2022-07-21 20:57:29 +0200

    Create insufficient permissions error page

---
+++ src/views/InsufficientPermissionsPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="app-page error-page insufficient-permissions">
+    <h3>Niewystarczające uprawnienia</h3>
+    <i class="ph-file-lock-light"></i>
+    <h5>Nie posiadasz dostępu do tej strony</h5>
+    <LinkButton to="/">Powrót na stronę główną</LinkButton>
+  </section>
+</template>
+
+<script>
+import LinkButton from '@/components/LinkButton.vue';
+
+export default {
+  name: 'InsufficientPermissionPage',
+  components: {
+    LinkButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.insufficient-permissions {
+  h3 {
+    font-size: 4rem;
+  }
+
+  h5 {
+    font-size: 2rem;
+  }
+
+  i {
+    font-size: 25rem;
+  }
+}
+</style>
