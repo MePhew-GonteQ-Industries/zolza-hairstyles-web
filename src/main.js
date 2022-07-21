2ad4a9aeba238613ff86a87f550af77fb89936cd
Me-Phew <basmateusz@wp.pl>
2022-07-21 18:20:41 +0200

    Initialize VWave

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import i18n from './i18n';
+import VWave from 'v-wave'
 
 const { VUE_APP_API_URL } = process.env;
 axios.defaults.baseURL = VUE_APP_API_URL;
@@ -15,4 +16,7 @@ createApp(App).use(i18n)
   .use(store)
   .use(router)
   .use(SetupCalendar, {})
+  .use(VWave, {
+    directive: 'ripple',
+  })
   .mount('#app');
