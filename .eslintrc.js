f48af38dd5265f4bc0828ecfee862029853857c5
Me-Phew <basmateusz@wp.pl>
2022-07-22 18:50:18 +0200

    Add validation options

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,29 @@ module.exports = {
     '@vue/airbnb',
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+    'vuejs-accessibility/label-has-for': [
+      'error',
+      {
+        components: ['VLabel'],
+        controlComponents: ['VInput'],
+        required: {
+          every: ['id'],
+        },
+        allowChildren: true,
+      },
+    ],
+    'vuejs-accessibility/anchor-has-content': [
+      'error',
+      {
+        components: ['Anchor'],
+        accessibleDirectives: ['aria-label'],
+      },
+    ],
   },
 };
