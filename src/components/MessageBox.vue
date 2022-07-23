958602a353edb853672032cc28518c9f8c94c3d9
Me-Phew <basmateusz@wp.pl>
2022-07-23 22:18:21 +0200

    Create custom message box component

--- src/components/MessageBox.vue
+++ src/components/MessageBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="message-box" ref="messageBox">
-    <i class="ph-warning-light"></i>
+    <i :class="iconClass"></i>
     <div class="heading">
       <p><slot name="title"></slot></p>
       <span><slot name="subtitle"></slot></span>
@@ -20,7 +20,9 @@ export default {
     },
   },
   setup(props) {
-    const messageBox = ref();
+    const messageBox = ref(null);
+
+    const iconClass = ref(null);
 
     onMounted(() => {
       let mainColor;
@@ -28,21 +30,25 @@ export default {
 
       switch (props.type) {
         case 'success': {
+          iconClass.value = 'ph-check-circle-light';
           mainColor = getCssPropertyValue('--success-color');
           accentColor = getCssPropertyValue('--success-color-low');
           break;
         }
         case 'info': {
+          iconClass.value = 'ph-info-light';
           mainColor = getCssPropertyValue('--info-color');
           accentColor = getCssPropertyValue('--info-color-low');
           break;
         }
         case 'warning': {
+          iconClass.value = 'ph-warning-circle-light';
           mainColor = getCssPropertyValue('--warning-color');
           accentColor = getCssPropertyValue('--warning-color-low');
           break;
         }
         case 'error': {
+          iconClass.value = 'ph-warning-light';
           mainColor = getCssPropertyValue('--error-color');
           accentColor = getCssPropertyValue('--error-color-low');
           break;
@@ -60,6 +66,7 @@ export default {
 
     return {
       messageBox,
+      iconClass,
     };
   },
 };
@@ -90,10 +97,15 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    gap: 1rem;
+    gap: .25rem;
 
     span {
       cursor: pointer;
+      font-size: .875rem;
+
+      &:hover {
+        text-decoration: underline;
+      }
     }
   }
 }
