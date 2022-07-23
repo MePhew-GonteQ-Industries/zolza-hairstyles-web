f826aa1b08ea5a65d763d0e602f9af1c1d9b4282
Me-Phew <basmateusz@wp.pl>
2022-07-23 16:13:58 +0200

    Remove conflicting rules

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,6 @@ module.exports = {
     config.plugin('define').tap((definitions) => {
       definitions[0] = Object.assign(definitions[0], {
         __VUE_I18N_FULL_INSTALL__: true,
-        __VUE_I18N_LEGACY_API__: false,
         __INTLIFY_PROD_DEVTOOLS__: false,
       });
       return definitions;
