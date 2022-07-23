6dc316d707bb27270375f2403753f9f7e6be3528
Me-Phew <basmateusz@wp.pl>
2022-07-23 16:15:50 +0200

    Add request error handler

--- src/utils.js
+++ src/utils.js
@@ -11,4 +11,24 @@ function setCssPropertyValue(element, propertyName, propertyValue) {
   element.style.setProperty(propertyName, propertyValue);
 }
 
-export { validateEmail, getCssPropertyValue, setCssPropertyValue };
+function handleRequestError(error) {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.error(error.response.status);
+    return error.response.status;
+  } if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.error(error.request);
+    return null;
+  }
+  // Something happened in setting up the request that triggered an Error
+  console.error(error.message);
+  return null;
+}
+
+export {
+  validateEmail, getCssPropertyValue, setCssPropertyValue, handleRequestError,
+};
