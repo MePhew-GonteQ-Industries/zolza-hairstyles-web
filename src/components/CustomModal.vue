3eac31a90b6d3ceb13375a35e8c8bb858230514b
Me-Phew <basmateusz@wp.pl>
2022-07-28 22:56:21 +0200

    Improve opening/closing logic and animations

--- src/components/CustomModal.vue
+++ src/components/CustomModal.vue
@@ -1,33 +1,60 @@
 <template>
   <teleport to="body">
-    <div
-      class="modal"
-      :class="{ open: open }"
-      ref="modal"
-      @click.self="handleModalClick"
-      @keyup.esc="handleModalClick"
-    >
-      <div class="modal-content" :class="{ shake: shake }">
-        <div class="header">
-          <div class="title"><slot name="title" /></div>
-          <i class="ph-x-light close-icon"></i>
+    <Transition v-if="open" appear>
+      <FocusTrap
+        :active="true"
+        :escapeDeactivates="false"
+        :initial-focus="$refs.modal"
+      >
+        <div
+          ref="modal"
+          class="modal"
+          :class="{ open: open, closing: closing }"
+          @click.self="handleModalClick"
+          @keyup.esc="handleModalClose"
+          tabindex="0"
+        >
+          <div
+            ref="modalContent"
+            class="modal-content"
+            :class="{ shake: shake }"
+          >
+            <div class="header">
+              <div class="title"><slot name="title" /></div>
+              <i
+                class="ph-x-light close-icon"
+                v-if="!persistent"
+                @click="closeModal"
+                @keyup.enter="closeModal"
+              ></i>
+            </div>
+            <div class="body">
+              <slot />
+            </div>
+          </div>
         </div>
-        <div class="body">
-          <slot />
-        </div>
-      </div>
-    </div>
+      </FocusTrap>
+    </Transition>
   </teleport>
 </template>
 
 <script>
 import { ref } from 'vue';
+import { FocusTrap } from 'focus-trap-vue';
 
 export default {
   name: 'CustomModal',
+  components: {
+    FocusTrap,
+  },
   props: {
     open: {
       type: Boolean,
+      required: true,
+    },
+    closeOnClickOutside: {
+      type: Boolean,
+      default: true,
     },
     persistent: {
       type: Boolean,
@@ -37,21 +64,45 @@ export default {
   emits: ['update:open'],
   setup(props, { emit }) {
     const shake = ref(false);
+    const closing = ref(false);
+
+    const closeModal = () => {
+      closing.value = true;
+      setTimeout(() => {
+        emit('update:open', false);
+        closing.value = false;
+      }, 400);
+    };
+
+    const shakeModal = () => {
+      shake.value = true;
+      setTimeout(() => {
+        shake.value = false;
+      }, 1000);
+    };
 
     const handleModalClick = () => {
-      if (!props.persistent) {
-        emit('update:open', false);
+      if (props.closeOnClickOutside && !props.persistent) {
+        closeModal();
       } else {
-        shake.value = true;
-        setTimeout(() => {
-          shake.value = false;
-        }, 1000);
+        shakeModal();
+      }
+    };
+
+    const handleModalClose = () => {
+      if (!props.persistent) {
+        closeModal();
+      } else {
+        shakeModal();
       }
     };
 
     return {
       handleModalClick,
+      handleModalClose,
       shake,
+      closing,
+      closeModal,
     };
   },
 };
@@ -66,15 +117,27 @@ export default {
   background-color: rgba(0, 0, 0, 0.4);
   z-index: 900;
   display: flex;
-  visibility: hidden;
   align-items: center;
   justify-content: center;
+  visibility: hidden;
 
   &.open {
     visibility: visible;
 
     .modal-content {
       transform: scale(1);
+    }
+
+    &.v-enter-from {
+      .modal-content {
+        transform: scale(0);
+      }
+    }
+
+    &.closing {
+      .modal-content {
+        transform: scale(0);
+      }
     }
   }
 
@@ -109,7 +172,7 @@ export default {
     box-shadow: 0 0 8px -2px $box-shadow-color;
     display: grid;
     grid-template-rows: 50px auto;
-    max-width: 50vw;
+    z-index: 1000;
 
     &.shake {
       animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
@@ -124,6 +187,7 @@ export default {
       grid-template-columns: auto 30px;
       align-items: center;
       padding: 0.5rem;
+      gap: 1rem;
 
       .close-icon {
         font-size: 1.5rem;
@@ -140,7 +204,7 @@ export default {
     }
 
     .body {
-      padding: 0.5rem;
+      padding: 1rem;
     }
   }
 }
