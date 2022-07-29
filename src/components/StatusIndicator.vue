ed9bc0b05ee05b80d6d67415c01b448095cc6953
Me-Phew <basmateusz@wp.pl>
2022-07-29 22:56:05 +0200

    Add status indicator component

---
+++ src/components/StatusIndicator.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="data-icon-wrapper">
+    <i
+      :class="[statusSuccess ? 'ph-check-circle-light' : 'ph-x-circle-light',
+      statusSuccess ? 'good' : 'wrong']"
+    >
+    </i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatusIndicator',
+  props: {
+    statusSuccess: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.data-icon-wrapper {
+  justify-content: center;
+
+  i {
+    background-color: $secondary-color;
+    padding: 0.25rem;
+    border-radius: 50%;
+
+    &.wrong {
+      background-color: $error-color-low;
+      color: $error-color;
+    }
+
+    &.good {
+      background-color: $success-color-low;
+      color: $success-color;
+    }
+  }
+}
+</style>
