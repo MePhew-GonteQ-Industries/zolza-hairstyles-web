1c02aad7787c23ef0d76136409d8df9381c9e522
Me-Phew <basmateusz@wp.pl>
2022-08-09 12:57:51 +0200

    Create custom components for settings page

---
+++ src/components/Settings/LoginSession.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="login-session">
+    <div class="session-data">
+      <div class="header">
+        <CustomTooltip type="info">
+          <template #activator>
+            <i
+              :class="
+                session.iconClass ? session.iconClass : 'ph-question-light'
+              "
+              class="device-icon"
+            ></i>
+          </template>
+          <span>{{ session.deviceTooltip }}</span>
+        </CustomTooltip>
+        <span class="location" v-if="session.last_access_data.location">
+          <span>{{ session.last_access_data.location.country }}</span
+          >, <span>{{ session.last_access_data.location.region }}</span>
+        </span>
+      </div>
+      <div class="content">
+        <span class="title">
+          <router-link :to="`sessions/${session.id}`"
+            ><span v-if="session.last_access_data.location">{{
+              session.last_access_data.location.city
+            }}</span>
+            <span
+              >&nbsp;{{ session.last_access_data.ip_address }}</span
+            ></router-link
+          >
+        </span>
+        <span class="last-access-notice">
+          <template v-if="session.id === $store.state.auth.sessionId"
+            >Twoja obecna sesja
+          </template>
+          <template v-else>
+            Ostatni dostęp {{ session.last_accessed_str }}
+          </template>
+        </span>
+      </div>
+    </div>
+    <LinkButton type="info" :to="`sessions/${session.id}`" class="details-link">
+      Szczegóły
+    </LinkButton>
+  </div>
+</template>
+
+<script>
+import LinkButton from '@/components/LinkButton.vue';
+import CustomTooltip from '@/components/CustomTooltip.vue';
+
+export default {
+  name: 'LoginSession',
+  components: {
+    LinkButton,
+    CustomTooltip,
+  },
+  props: {
+    session: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-session {
+  display: grid;
+  grid-template-columns: auto auto;
+  background-color: $background-accent-low;
+  padding: 1rem;
+  border-bottom: thin solid $secondary-color;
+  align-items: center;
+
+  &:hover {
+    background-color: $background-accent-medium;
+  }
+
+  .session-data {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    .header {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      .device-icon {
+        font-size: 3rem;
+        color: $secondary-text-color;
+      }
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+
+      .title {
+        a {
+          color: $secondary-text-color;
+          padding: 0;
+          font-size: 1.125rem;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+  }
+
+  .details-link {
+    width: 100px;
+    justify-self: flex-end;
+    height: 40px;
+  }
+}
+</style>
---
+++ src/components/Settings/ThemeCard.vue
@@ -0,0 +1,221 @@
+<template>
+  <div
+    class="theme-card"
+    @click="$emit('update:selectedTheme', themeName)"
+    @keypress.enter="$emit('update:selectedTheme', themeName)"
+  >
+    <CustomRadioButton
+      name="theme-selection"
+      :value="themeName"
+      :checked="selectedTheme === themeName"
+      @change="$emit('update:selectedTheme', themeName)"
+    >
+      <slot />
+    </CustomRadioButton>
+    <div class="theme-preview" ref="themePreview">
+      <div class="header">
+        <div class="accent-indicator"></div>
+        <div class="accent-line"></div>
+      </div>
+      <div class="main">
+        <div class="left">
+          <i class="ph-list-bullets-thin"></i>
+        </div>
+        <div class="right">
+          <i class="ph-dots-nine-thin"></i>
+        </div>
+      </div>
+      <div class="footer">
+        <i class="ph-atom-light"></i>
+        <div class="accent-line"></div>
+        <i class="ph-atom-light"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+import CustomRadioButton from '@/components/CustomRadioButton.vue';
+import { setCssPropertyValue, getCssPropertyValue } from '@/utils';
+
+export default {
+  name: 'ThemeCard',
+  components: {
+    CustomRadioButton,
+  },
+  props: {
+    themeName: {
+      type: String,
+      required: true,
+    },
+    selectedTheme: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['update:selectedTheme'],
+  setup(props) {
+    const themePreview = ref(null);
+
+    onMounted(() => {
+      const primaryColor = getCssPropertyValue(
+        `--primary-color-${props.themeName}`,
+      );
+      const secondaryColor = getCssPropertyValue(
+        `--secondary-color-${props.themeName}`,
+      );
+      const accentColor = getCssPropertyValue(
+        `--accent-color-${props.themeName}`,
+      );
+      const primaryTextColor = getCssPropertyValue(
+        `--primary-text-color-${props.themeName}`,
+      );
+      const secondaryTextColor = getCssPropertyValue(
+        `--secondary-text-color-${props.themeName}`,
+      );
+      const themeColor = getCssPropertyValue(
+        `--theme-color-${props.themeName}`,
+      );
+
+      setCssPropertyValue(
+        themePreview.value,
+        '--primary-theme-color',
+        primaryColor,
+      );
+      setCssPropertyValue(
+        themePreview.value,
+        '--secondary-theme-color',
+        secondaryColor,
+      );
+      setCssPropertyValue(
+        themePreview.value,
+        '--accent-theme-color',
+        accentColor,
+      );
+      setCssPropertyValue(
+        themePreview.value,
+        '--primary-text-theme-color',
+        primaryTextColor,
+      );
+      setCssPropertyValue(
+        themePreview.value,
+        '--secondary-text-theme-color',
+        secondaryTextColor,
+      );
+      setCssPropertyValue(themePreview.value, '--theme-color', themeColor);
+    });
+
+    return {
+      themePreview,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.theme-card {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  cursor: pointer;
+}
+
+.theme-preview {
+  $primary-theme-color: var(--primary-theme-color);
+  $secondary-theme-color: var(--secondary-theme-color);
+  $accent-theme-color: var(--accent-theme-color);
+  $primary-text-theme-color: var(--primary-text-theme-color);
+  $secondary-text-theme-color: var(--secondary-text-theme-color);
+  $theme-color: var(--theme-color);
+
+  padding: 1rem;
+  gap: 1rem;
+  background-color: $theme-color;
+  height: 400px;
+  width: 600px;
+  border-radius: 0.313rem;
+  display: flex;
+  flex-direction: column;
+
+  * {
+    border-radius: inherit;
+  }
+
+  .accent-indicator {
+    height: 12px;
+    width: 12px;
+    border-radius: 50%;
+    background-color: $accent-theme-color;
+  }
+
+  .accent-line {
+    height: 2px;
+    width: 100%;
+    background-color: $primary-text-theme-color;
+  }
+
+  .header,
+  .footer {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 0 2rem;
+  }
+
+  .header {
+    height: 20%;
+    width: 100%;
+    background-color: $primary-theme-color;
+
+    .accent-line {
+      background-color: $secondary-text-theme-color;
+    }
+  }
+
+  .main {
+    display: flex;
+    gap: 1rem;
+    height: 60%;
+    width: 100%;
+    font-size: 8rem;
+    color: $primary-text-theme-color;
+
+    .left,
+    .right {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-evenly;
+      gap: 1rem;
+      padding: 0 2rem;
+    }
+
+    .left {
+      height: 100%;
+      width: 65%;
+      background-color: $secondary-theme-color;
+      align-items: flex-start;
+      font-size: 12rem;
+    }
+
+    .right {
+      height: 100%;
+      width: 35%;
+      background-color: $primary-theme-color;
+    }
+  }
+
+  .footer {
+    height: 20%;
+    width: 100%;
+    background-color: $secondary-theme-color;
+    font-size: 1.5rem;
+    color: $accent-theme-color;
+
+    .accent-line {
+      background-color: $secondary-text-theme-color;
+    }
+  }
+}
+</style>
