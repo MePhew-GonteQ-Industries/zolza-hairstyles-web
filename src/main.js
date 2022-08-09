8583f85d62babba4cef1c900cd9fa8e205c5b5a1
Me-Phew <basmateusz@wp.pl>
2022-08-09 12:43:35 +0200

    Use relative imports

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@
 import { createApp } from 'vue';
 import axios from 'axios';
 import { SetupCalendar } from 'v-calendar';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import i18n from './i18n';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import i18n from '@/i18n';
 import VWave from 'v-wave';
 
 const { VUE_APP_API_URL } = process.env;
