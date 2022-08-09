30e0a7de58093ff458914dd3338da893ad11142e
Me-Phew <basmateusz@wp.pl>
2022-08-09 12:55:01 +0200

    Move time ago functionality to seperate module

---
+++ src/timeAgo.js
@@ -0,0 +1,9 @@
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en.json';
+import pl from 'javascript-time-ago/locale/pl.json';
+
+TimeAgo.addLocale(pl);
+TimeAgo.addLocale(en);
+TimeAgo.setDefaultLocale(pl.locale);
+
+export default TimeAgo;
