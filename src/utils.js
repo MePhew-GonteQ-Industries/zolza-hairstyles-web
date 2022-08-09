48352529e34b2a26c5fac65f5082c99da72abe48
Me-Phew <basmateusz@wp.pl>
2022-08-09 12:51:29 +0200

    Improve error handling

--- src/utils.js
+++ src/utils.js
@@ -15,18 +15,15 @@ function handleRequestError(error) {
   if (error.response) {
     // The request was made and the server responded with a status code
     // that falls out of the range of 2xx
-    console.error(error.response.status);
-    return error.response.status;
+    return error.response;
   } if (error.request) {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
     // http.ClientRequest in node.js
-    console.error(error.request);
-    return null;
+    throw error;
   }
   // Something happened in setting up the request that triggered an Error
-  console.error(error.message);
-  return null;
+  throw error;
 }
 
 export {
