819fb28d871c68618a54ff5805f92596558fdb99
Me-Phew <basmateusz@wp.pl>
2022-08-09 13:01:07 +0200

    Register sessions module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from 'vuex';
 import auth from '@/store/modules/auth';
 import user from '@/store/modules/user';
 import settings from '@/store/modules/settings';
+import sessions from '@/store/modules/sessions';
 
 export default createStore({
   modules: {
     auth,
     user,
     settings,
+    sessions,
   },
 });
