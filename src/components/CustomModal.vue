fbeee279ce364dbcb847dcc5478c1c43c57d703e
Me-Phew <basmateusz@wp.pl>
2022-09-03 13:34:25 +0200

    Improve styling

--- src/components/CustomModal.vue
+++ src/components/CustomModal.vue
@@ -188,6 +188,9 @@ export default {
     display: grid;
     grid-template-rows: 50px auto;
     z-index: 1000;
+    max-height: 80%;
+    max-width: 50%;
+    overflow: auto;
 
     &.shake {
       animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
