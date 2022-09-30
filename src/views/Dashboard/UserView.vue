ac1677d07b7c3e1b1670fdad658528789796208c
Me-Phew <basmateusz@wp.pl>
2022-09-30 22:42:15 +0200

    Create user view page

--- src/views/Dashboard/UserView.vue
+++ src/views/Dashboard/UserView.vue
@@ -1,12 +1,44 @@
 <template>
-<div class="dashboard-page appointment-view">
-  {{ $route.params.id }}
-</div>
+  <div class="dashboard-page user-view">
+    {{ $route.params.id }}
+    {{ userData }}
+  </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+import { useStore } from 'vuex';
+import { useRoute } from 'vue-router';
+import axios from 'axios';
+import { handleRequestError } from '@/utils';
+
 export default {
-  name: 'AppointmentView',
+  name: 'UserView',
+  setup() {
+    const store = useStore();
+    const route = useRoute();
+
+    const userData = ref(null);
+
+    onMounted(async () => {
+      const storedUser = store.getters.getUserById(route.params.id);
+
+      if (storedUser) {
+        userData.value = storedUser;
+      } else {
+        try {
+          const response = await axios.get(`users/${route.params.id}`);
+          userData.value = response.data;
+        } catch (error) {
+          handleRequestError(error);
+        }
+      }
+    });
+
+    return {
+      userData,
+    };
+  },
 };
 </script>
 
