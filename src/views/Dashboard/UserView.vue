7dcf89153c67df43e8dbc8bdc3b908d3b842ab59
biQte <patrykgonet@gmail.com>
2022-10-28 14:49:09 +0200

    Add owner/user label

--- src/views/Dashboard/UserView.vue
+++ src/views/Dashboard/UserView.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="dashboard-page user-view" v-if="userData">
     <!-- {{ $route.params.id }} -->
-    <p>ID użytkownika: {{ userData.id }}</p>
-    <p>Imie: {{ userData.name }}</p>
-    <p>Naziwsko: {{ userData.surname }}</p>
-    <p>Płeć: {{ userData.gender }}</p>
-    <p>Email: {{ userData.email }}</p>
-    <p>Zweryfikowany: {{ userData.verified }}</p>
-    <p>Wyłączony: {{ userData.disabled }}</p>
+    <div class="user elevated-card">
+      <div class="avatar-row">
+        <i class="ph-user-square-light avatar icon"></i>
+        <div class="user-data">
+          <CustomChip type="info" :customIconClass="userIconClass">{{ userRole }}</CustomChip>
+          <p class="email">{{ userData.email }}</p>
+        </div>
+      </div>
+      <p>ID użytkownika: {{ userData.id }}</p>
+      <p>Imie: {{ userData.name }}</p>
+      <p>Naziwsko: {{ userData.surname }}</p>
+      <p>Płeć: {{ userData.gender }}</p>
+      <p>Email: {{ userData.email }}</p>
+      <p>Zweryfikowany: {{ userData.verified }}</p>
+      <p>Wyłączony: {{ userData.disabled }}</p>
+    </div>
   </div>
 </template>
 
@@ -17,14 +26,21 @@ import { useStore } from "vuex";
 import { useRoute } from "vue-router";
 import axios from "axios";
 import { handleRequestError } from "@/utils";
+import CustomChip from "@/components/CustomChip.vue";
 
 export default {
   name: "UserView",
+  components: {
+    CustomChip,
+  },
   setup() {
     const store = useStore();
     const route = useRoute();
 
     const userData = ref(null);
+
+    const userRole = ref(null);
+    let userIconClass;
 
     onMounted(async () => {
       const storedUser = store.getters.getUserById(route.params.id);
@@ -39,13 +55,28 @@ export default {
           handleRequestError(error);
         }
       }
+
+      if (userData.value.isOwner) {
+        userIconClass = "ph-user-gear-light";
+        userRole.value = "Właściciel";
+      } else if (userData.value.isAdmin) {
+        userIconClass = "ph-wrench-light";
+        userRole.value = "Admin";
+      } else {
+        userIconClass = "ph-user-light";
+        userRole.value = "Użytkownik";
+      }
     });
 
     return {
       userData,
+      userRole,
+      userIconClass,
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
