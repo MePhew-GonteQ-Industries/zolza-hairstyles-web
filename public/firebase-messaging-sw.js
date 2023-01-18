ea415ca367a4811b4b6ab4608a2a1c79cd2dbd35
Me-Phew <basmateusz@wp.pl>
2023-01-18 17:54:40 +0100

    Fix corrupted file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -13,16 +13,17 @@ clientsClaim();
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp({
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyDD7fUs6hQ0wmc4DyDlSXEnqJVgb-8d92w",
+  authDomain: "zolza-hairstyles.firebaseapp.com",
+  projectId: "zolza-hairstyles",
+  storageBucket: "zolza-hairstyles.appspot.com",
+  messagingSenderId: "436999151575",
+  appId: "1:436999151575:web:5998a5ea066c09d1c33592",
+  measurementId: "G-JFC9R9LRXY"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
