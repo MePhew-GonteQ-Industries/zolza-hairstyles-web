4b2ae73a3838923450fd4a40be1844e95de100e4
Me-Phew <basmateusz@wp.pl>
2023-05-25 19:22:24 +0200

    Add creator message

--- src/main.js
+++ src/main.js
@@ -11,6 +11,18 @@ import VWave from "v-wave";
 const { VITE_API_URL } = import.meta.env;
 axios.defaults.baseURL = VITE_API_URL;
 
+const printCreatorMessage = () => {
+  if (navigator.userAgent.toLowerCase().includes("chrome")) {
+    const n = [`
+    %c Made with \u2665 by MePhew & GonteQ Industries %c https://github.com/MePhew-GonteQ-Industries`,
+      "color: #fff; background: #19A7CE; padding:5px 0;", "color: #fff; background: #1c1c1c; padding:5px 0;"];
+    window.console.log.apply(console, n)
+  } else
+    window.console && window.console.log("Made with \u2665 by MePhew & GonteQ Industries - https://github.com/MePhew-GonteQ-Industries")
+}
+
+printCreatorMessage()
+
 createApp(App)
   .use(i18n)
   .use(store)
